d760d1f0bf57178770aa5067972ce89c4e1fc617
杨浩 <yanghaogn@163.com>
2014-10-23 18:17:48 +0800

    add code

---
+++ src/Binary Tree Level Order Traversal/Solution.java
@@ -0,0 +1,51 @@
+class TreeNode
+{
+    int val;
+
+    TreeNode left;
+
+    TreeNode right;
+
+    TreeNode(int x)
+    {
+        val = x;
+    }
+}
+
+
+public class Solution
+{
+    public List<List<Integer>> levelOrder(TreeNode root)
+    {
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+        if (root == null)
+        {
+            return result;
+        }
+        List<TreeNode> first = new LinkedList<TreeNode>();
+        List<TreeNode> second = new LinkedList<TreeNode>();
+        first.add(root);
+        while (first.size() > 0)
+        {
+            LinkedList<Integer> currentLevel = new LinkedList<Integer>();
+            for (TreeNode node : first)
+            {
+                currentLevel.add(node.val);
+                if (node.left != null)
+                {
+                    second.add(node.left);
+                }
+                if (node.right != null)
+                {
+                    second.add(node.right);
+                }
+            }
+            result.add(currentLevel);
+            List<TreeNode> tmp = first;
+            first = second;
+            second = tmp;
+            second.clear();
+        }
+        return result;
+    }
+}
