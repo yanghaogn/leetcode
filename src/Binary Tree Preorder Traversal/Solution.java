77684771de18bfd6e59fcee7563b3b009b6d089a
杨浩 <yanghaogn@163.com>
2014-11-01 16:53:06 +0800

    Update Solution.java

--- src/Binary Tree Preorder Traversal/Solution.java
+++ src/Binary Tree Preorder Traversal/Solution.java
@@ -12,41 +12,16 @@ public class TreeNode
     }
 }
 
-
-public class Solution
-{
-    public List<Integer> preorderTraversal(TreeNode root)
-    {
-        List<Integer> result = null;
-        if (root == null)
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result=new LinkedList<Integer>();
+        if(root==null)
         {
-            result = new LinkedList<Integer>();
             return result;
         }
-        if (root.left == null && root.right == null)
-        {
-            result = new LinkedList<Integer>();
-            result.add(root.val);
-            return result;
-        }
-
-        if (root.left != null)
-        {
-            result = preorderTraversal(root.left);
-        }
-        if (root.right != null)
-        {
-            List<Integer> right = preorderTraversal(root.right);
-            if (result == null)
-            {
-                result = right;
-            }
-            else
-            {
-                result.addAll(right);
-            }
-        }
-        result.add(0, root.val);
+        result.add(root.val);
+        result.addAll(preorderTraversal(root.left));
+        result.addAll(preorderTraversal(root.right));
         return result;
     }
 }
