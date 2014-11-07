980b3e1384ba3282d56f65c1efe5cb3054cf6b71
杨浩 <yanghaogn@163.com>
2014-11-07 11:36:23 +0800

    Update Solution.java

--- src/3Sum Closest/Solution.java
+++ src/3Sum Closest/Solution.java
@@ -1,56 +1,5 @@
 public class Solution
 {
-    public void swap(int[] array, int i, int j)
-    {
-        int t = array[i];
-        array[i] = array[j];
-        array[j] = t;
-    }
-
-    public void quickSort(int[] array, int start, int end)
-    {
-        if (start >= end)
-        {
-            return;
-        }
-        int middle = (start + end) / 2;
-        swap(array, start, middle);
-        int i = start + 1;
-        while (i <= end)
-        {
-            if (array[i] >= array[start])
-            {
-                break;
-            }
-            i++ ;
-        }
-        int j = i + 1;
-        for (; j <= end;)
-        {
-            while (j <= end)
-            {
-                if (array[j] < array[start])
-                {
-                    break;
-                }
-                j++ ;
-            }
-            if (j <= end)
-            {
-                swap(array, i, j);
-                i++ ;
-                j++ ;
-            }
-        }
-        i-- ;
-        if (array[i] < array[start])
-        {
-            swap(array, start, i);
-        }
-        quickSort(array, start, i - 1);
-        quickSort(array, i + 1, end);
-    }
-
     public int threeSumClosest(int[] num, int target)
     {
         int result = num[0] + num[1] + num[2];
@@ -60,7 +9,7 @@ public class Solution
         int i;
         int j;
         int k;
-        quickSort(num, 0, num.length - 1);
+        Arrays.sort(num);
         for (i = 0; i < num.length - 2; i++ )
         {
             j = i + 1;
